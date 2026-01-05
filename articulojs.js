c9f0aa6b9cdc9ef7ed51ce9910976c40943fdf05
Alejandro Monterroso <alejandromonterrosoc@gmail.com>
2026-01-05 16:58:21 -0600

    Article page dinamic content
    Creation of the dinamic content in the article page, changes in h1, h2.......h4 font sizes in big screens.

--- articulojs.js
+++ articulojs.js
@@ -1,47 +1,52 @@
-const articulos = {
-  art1: {
+const articles = [
+  {
     id: 1,
-    title: 'Tipos de Tela para Uniformes Empresariales',
-    imgs: ['imgsCarpet/articleImgs/telasArticleImgs/telaAlgodon.jpg', 'imgsCarpet/articleImgs/telasArticleImgs/telaAntiManchas.jpg', 'imgsCarpet/articleImgs/telasArticleImgs/telaImpermeable.jpg'],
-    prg1: `<h4 class="articulo-subtitle">Durabilidad, Calidad y Usos Recomendados</h4>
+    title: 'Tipos de Tela para Uniformes Empresariales',    
+    content: `<h4 class="articulo-subtitle">Durabilidad, Calidad y Usos Recomendados</h4>
     <p class="articulo-p">La selección adecuada de la tela es fundamental al diseñar uniformes empresariales, ya que determina no solo la estética del uniforme, sino también su desempeño, durabilidad y confort. Un uniforme bien fabricado debe resistir la rutina diaria, mantener una buena presentación y brindar comodidad al colaborador, por lo que entender las características de cada tipo de tela es un paso esencial para cualquier empresa que busque calidad.</p>
     <h4 class="articulo-subtitle">Poliéster</h4>
-    <p class="articulo-p">El poliéster es una de las telas más resistentes para uniformes, gracias a su durabilidad, firmeza y capacidad para conservar los colores por más tiempo. No se arruga con facilidad y necesita poco mantenimiento, lo que lo convierte en una elección ideal para empresas que buscan uniformes de larga vida útil. Es perfecto para entornos donde los colaboradores están en constante movimiento o expuestos a desgaste físico. Una de las ventajas de nuestras telas es que trae protección solar y control de humedad, y  repelencia al agua.</p>`,
-    prg2: `<h4 class="articulo-subtitle" class="articulo-subtitle">Poliéster y Algodón</h4>
+    <p class="articulo-p">El poliéster es una de las telas más resistentes para uniformes, gracias a su durabilidad, firmeza y capacidad para conservar los colores por más tiempo. No se arruga con facilidad y necesita poco mantenimiento, lo que lo convierte en una elección ideal para empresas que buscan uniformes de larga vida útil. Es perfecto para entornos donde los colaboradores están en constante movimiento o expuestos a desgaste físico. Una de las ventajas de nuestras telas es que trae protección solar y control de humedad, y  repelencia al agua.</p>
+    <img src="imgsCarpet/articleImgs/telasArticleImgs/telaAlgodon.jpg" alt="Poliester tela" class="articulo-img">
+    <h4 class="articulo-subtitle" class="articulo-subtitle">Poliéster y Algodón</h4>
     <p class="articulo-p">La combinación de algodón y poliéster logra un equilibrio excepcional entre comodidad y durabilidad. Esta mezcla resiste mejor el lavado continuo, mantiene una buena apariencia y se siente más ligera y suave. Es una de las alternativas más utilizadas en uniformes empresariales debido a su rendimiento general. Su costo suele ser ligeramente mayor que el de una tela 100% algodón, pero la inversión se ve compensada por su vida útil superior.</p>
     <h4 class="articulo-subtitle"><h4 class="articulo-subtitle">Telas antifluido</h4>
     <p class="articulo-p">por su construcción con microfibra y protector textil que repele los líquidos y la mugre, te protegen de salpicaduras evitando que tu cuerpo entre en contacto con estos. Sus acabados especiales protegen la tela de manchas y hacen mucho más fácil el cuidado de las mismas, además de su agradable sensación al tacto.</p>
     <h4 class="articulo-subtitle">Tela strech</h4>
-    <p class="articulo-p">Bases que por su strech mecánico permite mayor flexibilidad dando más libertad de movimiento comodidad y ajuste perfecto de la prenda.</p>`,
-    prg3: `<h4 class="articulo-subtitle">Telas impermeables</h4>
+    <p class="articulo-p">Bases que por su strech mecánico permite mayor flexibilidad dando más libertad de movimiento comodidad y ajuste perfecto de la prenda.</p>
+    <img src="imgsCarpet/articleImgs/telasArticleImgs/telaAntiManchas.jpg" alt="Poliester tela" class="articulo-img">
+    <h4 class="articulo-subtitle">Telas impermeables</h4>
     <p class="articulo-p">Gracias a su impermeabilidad te sentirás cómodo y protegido de factores ambientales externos como el agua y el frio</p>
     <h4 class="articulo-subtitle">Tejido de punto</h4>
     <p class="articulo-p">Son bases de alto desempeño que gracias a su tecnología absorben, secan y expulsan la humedad del cuerpo generando una sensación de frescura y comodidad. su contracción hace que tenga buena flexibilidad y se sientan suaves al contacto con la piel.</p>
     <h4 class="articulo-subtitle">Tejido liviano</h4>
-    <p class="articulo-p">Son telas para camisa que son livianos a base de algodón y poliéster, con tacto suave y muy cómodo.</p>`
+    <p class="articulo-p">Son telas para camisa que son livianos a base de algodón y poliéster, con tacto suave y muy cómodo.</p>
+    <img src="imgsCarpet/articleImgs/telasArticleImgs/telaImpermeable.jpg" alt="Poliester tela" class="articulo-img">`,
+    date: '05/01/2026'
   },
-  art2: {
+  {
     id: 2,
-    title: 'Cómo cuidar tu uniforme empresarial para mantenerlo impecable',
-    imgs: ['imgsCarpet/articleImgs/telasArticleImgs/telaAlgodon.jpg', 'imgsCarpet/articleImgs/telasArticleImgs/telaAntiManchas.jpg', 'imgsCarpet/articleImgs/telasArticleImgs/telaImpermeable.jpg'],
-    prg1: `<h4 class="articulo-subtitle">Lavado correcto</h4>
+    title: 'Cuidado adecuado del uniforme empresarial',    
+    content: `<h4 class="articulo-subtitle">Lavado correcto</h4>
     <p class="articulo-p">El primer paso para conservar un uniforme empresarial en buen estado es el lavado. Utiliza ciclos suaves y evita temperaturas muy altas, ya que el calor excesivo puede dañar las fibras y alterar los colores. Los detergentes neutros son ideales para preservar la tonalidad original y prolongar la vida útil de las telas.</p><h4 class="articulo-subtitle">Secado Responsable</h4>
-    <p class="articulo-p">El secado es otro aspecto clave. Evita la exposición directa al sol durante largos periodos, pues esto puede debilitar las fibras y desvanecer los tonos. Lo más recomendable es secar a la sombra o en espacios ventilados, lo que garantiza frescura y durabilidad.</p>`,
-    prg2: `<h4 class="articulo-subtitle">Planchado y presentación</h4>
+    <p class="articulo-p">El secado es otro aspecto clave. Evita la exposición directa al sol durante largos periodos, pues esto puede debilitar las fibras y desvanecer los tonos. Lo más recomendable es secar a la sombra o en espacios ventilados, lo que garantiza frescura y durabilidad.</p>
+    <img src="imgsCarpet/articleImgs/telasArticleImgs/telaAlgodon.jpg" alt="Poliester tela" class="articulo-img">
+    <h4 class="articulo-subtitle">Planchado y presentación</h4>
     <p class="articulo-p">Un uniforme bien planchado transmite profesionalismo. Ajusta la temperatura de la plancha según el tipo de tela: algodón requiere calor moderado, mientras que las fibras sintéticas necesitan temperaturas más bajas para evitar daños. Este cuidado asegura una apariencia impecable en todo momento.</p><h4 class="articulo-subtitle">Almacenamiento adecuado</h4>
-    <p class="articulo-p">Guardar el uniforme correctamente es esencial. Utiliza perchas para conservar la forma y evita espacios húmedos que puedan generar malos olores o deterioro. Un almacenamiento limpio y ordenado refleja el mismo nivel de profesionalismo que se espera de la empresa.</p>`,
-    prg3: `<h4 class="articulo-subtitle">Imagen institucional</h4>
-    <p class="articulo-p">El uniforme no es solo una prenda, es parte de la identidad corporativa. Mantenerlo en buen estado refuerza la imagen institucional, proyecta confianza y demuestra respeto hacia la marca y hacia quienes lo usan.</p>`
+    <p class="articulo-p">Guardar el uniforme correctamente es esencial. Utiliza perchas para conservar la forma y evita espacios húmedos que puedan generar malos olores o deterioro. Un almacenamiento limpio y ordenado refleja el mismo nivel de profesionalismo que se espera de la empresa.</p>
+    <img src="imgsCarpet/articleImgs/telasArticleImgs/telaAntiManchas.jpg" alt="Poliester tela" class="articulo-img">
+    <h4 class="articulo-subtitle">Imagen institucional</h4>
+    <p class="articulo-p">El uniforme no es solo una prenda, es parte de la identidad corporativa. Mantenerlo en buen estado refuerza la imagen institucional, proyecta confianza y demuestra respeto hacia la marca y hacia quienes lo usan.</p>`,
+    date: '05/01/2026'
   },
-  art3: {
+  {
     id: 3,
-    title: 'Telas ideales para uniformes empresariales en distintos climas',
-    imgs: ['imgsCarpet/articleImgs/telasArticleImgs/telaAlgodon.jpg', 'imgsCarpet/articleImgs/telasArticleImgs/telaAntiManchas.jpg', 'imgsCarpet/articleImgs/telasArticleImgs/telaImpermeable.jpg'],
-    prg1: `<h4 class="articulo-subtitle">elección correcta</h4>
-    <p class="articulo-p">La elección de la tela para un uniforme empresarial es un factor clave que impacta en la comodidad del trabajador, la durabilidad de la prenda y la imagen institucional de la empresa. No todas las telas funcionan igual en diferentes ambientes: mientras que en zonas cálidas se busca frescura y transpirabilidad, en climas fríos la prioridad es conservar el calor sin perder elegancia.</p>`,
-    prg2: `<h4 class="articulo-subtitle">Telas para climas cálidos y costeros</h4>
-    <p class="articulo-p">En regiones con altas temperaturas y humedad, los uniformes deben ser ligeros y permitir la circulación del aire. El algodón y el lino son opciones naturales que aportan frescura, mientras que las mezclas sintéticas con tecnología anti sudor ofrecen resistencia y secado rápido.<br>En ambientes fríos, los uniformes deben proteger contra bajas temperaturas y mantener una apariencia profesional. Las mezclas de algodón con poliéster grueso, la lana ligera y los tejidos técnicos como el softshell son ideales para conservar calor y resistir condiciones adversas.</p>`,
-    prg3: `
+    title: 'Telas ideales para distintos climas',    
+    content: `<h4 class="articulo-subtitle">elección correcta</h4>
+    <p class="articulo-p">La elección de la tela para un uniforme empresarial es un factor clave que impacta en la comodidad del trabajador, la durabilidad de la prenda y la imagen institucional de la empresa. No todas las telas funcionan igual en diferentes ambientes: mientras que en zonas cálidas se busca frescura y transpirabilidad, en climas fríos la prioridad es conservar el calor sin perder elegancia.</p>
+    <img src="imgsCarpet/articleImgs/telasArticleImgs/telaAntiManchas.jpg" alt="Poliester tela" class="articulo-img">
+    <h4 class="articulo-subtitle">Telas para climas cálidos y costeros</h4>
+    <p class="articulo-p">En regiones con altas temperaturas y humedad, los uniformes deben ser ligeros y permitir la circulación del aire. El algodón y el lino son opciones naturales que aportan frescura, mientras que las mezclas sintéticas con tecnología anti sudor ofrecen resistencia y secado rápido.<br>En ambientes fríos, los uniformes deben proteger contra bajas temperaturas y mantener una apariencia profesional. Las mezclas de algodón con poliéster grueso, la lana ligera y los tejidos técnicos como el softshell son ideales para conservar calor y resistir condiciones adversas.</p>
+    <img src="imgsCarpet/articleImgs/telasArticleImgs/telaAntiManchas.jpg" alt="Poliester tela" class="articulo-img">
     <table>
       <caption>
         Tabla comparativa de telas por clima
@@ -67,19 +72,56 @@ const articulos = {
       </tbody>      
     </table>
     <h4 class="articulo-subtitle">Conclusión</h4>
-    <p class="articulo-p">Seleccionar la tela adecuada según el clima garantiza que los uniformes empresariales sean cómodos, duraderos y proyecten la identidad de la marca. Adaptar materiales a cada entorno no solo mejora el rendimiento del equipo, sino que refuerza la confianza y profesionalismo de la empresa.</p>`
+    <p class="articulo-p">Seleccionar la tela adecuada según el clima garantiza que los uniformes empresariales sean cómodos, duraderos y proyecten la identidad de la marca. Adaptar materiales a cada entorno no solo mejora el rendimiento del equipo, sino que refuerza la confianza y profesionalismo de la empresa.</p>
+    <img src="imgsCarpet/articleImgs/telasArticleImgs/telaAlgodon.jpg" alt="Poliester tela" class="articulo-img">`,
+    date: '05/01/2026'
   },
-  art4: {
+  {
     id: 4,
-    title: 'El poder del uniforme empresarial en la construcción de imagen corporativa',
-    imgs: ['imgsCarpet/articleImgs/telasArticleImgs/telaAlgodon.jpg', 'imgsCarpet/articleImgs/telasArticleImgs/telaAntiManchas.jpg', 'imgsCarpet/articleImgs/telasArticleImgs/telaImpermeable.jpg'],
-    prg1: `<h4 class="articulo-subtitle">Más que una prenda: una declaración de marca</h4>
-    <p class="articulo-p">El uniforme empresarial no es solo una vestimenta funcional; es una herramienta estratégica que comunica profesionalismo, coherencia y pertenencia. Cuando los colaboradores visten prendas alineadas con la identidad visual de la empresa, se proyecta una imagen sólida y confiable ante clientes, proveedores y aliados.<br>Según el artículo de Taller de Comunicación, los uniformes son una extensión visual de la marca, capaces de generar reconocimiento inmediato y reforzar los valores institucionales. Esta coherencia visual contribuye a que la empresa sea percibida como organizada, seria y comprometida.</p>`,
-    prg2: `<h4 class="articulo-subtitle">Impacto en la percepción del cliente</h4>
+    title: 'Uniforme empresarial y la imagen corporativa',    
+    content: `<h4 class="articulo-subtitle">Más que una prenda: una declaración de marca</h4>
+    <p class="articulo-p">El uniforme empresarial no es solo una vestimenta funcional; es una herramienta estratégica que comunica profesionalismo, coherencia y pertenencia. Cuando los colaboradores visten prendas alineadas con la identidad visual de la empresa, se proyecta una imagen sólida y confiable ante clientes, proveedores y aliados.<br>Según el artículo de Taller de Comunicación, los uniformes son una extensión visual de la marca, capaces de generar reconocimiento inmediato y reforzar los valores institucionales. Esta coherencia visual contribuye a que la empresa sea percibida como organizada, seria y comprometida.</p>
+    <img src="imgsCarpet/articleImgs/telasArticleImgs/telaAlgodon.jpg" alt="Poliester tela" class="articulo-img">
+    <h4 class="articulo-subtitle">Impacto en la percepción del cliente</h4>
     <p class="articulo-p">Estudios recientes en España muestran que las empresas que invierten en moda corporativa logran una mejora significativa en la percepción del cliente. Un uniforme bien diseñado transmite orden, limpieza y atención al detalle, lo que influye directamente en la experiencia del consumidor.<br>Además, el uso de uniformes facilita la identificación del personal, agiliza la atención y genera confianza. En sectores como retail, salud, hotelería y servicios, esta diferenciación visual es clave para destacar frente a la competencia.</p>
     <h4 class="articulo-subtitle">Cohesión interna y sentido de pertenencia</h4>
-    <p class="articulo-p">A nivel interno, el uniforme empresarial fortalece el sentido de equipo. Los colaboradores se sienten parte de una organización que cuida su imagen y valora su rol. Esto puede traducirse en mayor compromiso, disciplina y orgullo por representar la marca.<br>La agencia Wolf Marketing destaca que los uniformes generan una conexión emocional con la empresa, reforzando la cultura corporativa y mejorando la comunicación visual entre departamentos.</p>`,
-    prg3: `<h4 class="articulo-subtitle">Una inversión que tu empresa necesita</h4>
-    <p class="articulo-p">Invertir en uniformes empresariales es una decisión inteligente que combina estética, funcionalidad y estrategia. Empresas que apuestan por prendas bien diseñadas y duraderas no solo elevan su <strong>imagen institucional</strong>, sino que también proyectan confianza y profesionalismo en cada interacción.<br>En Sayros Uniformes, entendemos que cada detalle cuenta. Por eso ofrecemos soluciones que combinan <strong>diseño</strong>, <strong>estilo e imagen</strong>, adaptadas a las necesidades reales de tu empresa.</p>`
+    <p class="articulo-p">A nivel interno, el uniforme empresarial fortalece el sentido de equipo. Los colaboradores se sienten parte de una organización que cuida su imagen y valora su rol. Esto puede traducirse en mayor compromiso, disciplina y orgullo por representar la marca.<br>La agencia Wolf Marketing destaca que los uniformes generan una conexión emocional con la empresa, reforzando la cultura corporativa y mejorando la comunicación visual entre departamentos.</p>
+    <img src="imgsCarpet/articleImgs/telasArticleImgs/telaAlgodon.jpg" alt="Poliester tela" class="articulo-img">
+    <h4 class="articulo-subtitle">Una inversión que tu empresa necesita</h4>
+    <p class="articulo-p">Invertir en uniformes empresariales es una decisión inteligente que combina estética, funcionalidad y estrategia. Empresas que apuestan por prendas bien diseñadas y duraderas no solo elevan su <strong>imagen institucional</strong>, sino que también proyectan confianza y profesionalismo en cada interacción.<br>En Sayros Uniformes, entendemos que cada detalle cuenta. Por eso ofrecemos soluciones que combinan <strong>diseño</strong>, <strong>estilo e imagen</strong>, adaptadas a las necesidades reales de tu empresa.</p>`,
+    date: '05/01/2026'
   }
+]
+const artsWrapper = document.querySelector('.articulos-wrapper');
+const urlInfo = new URLSearchParams(window.location.search);
+const artId = urlInfo.get('id');
+if (artsWrapper) {
+  displayArticles(articles);
+} else if (artId) {
+  showArticle(articles);
+}
+
+function displayArticles(arts) {
+  let artsBox = arts.map((art) => {
+    return `<div class="articulos-item-wrapper">
+        <a href="articulo.html?id=${art.id}" class="articulos-links d-flex-wrapper">
+          <div class="articulo-numero-wrapper d-flex-wrapper">${art.id}</div>
+          <div class="articulo-texto-wrapper">
+            <h3 class="articulo-texto-title">${art.title}</h3>
+            <span class="articulo-fecha-texto">Publicado: ${art.date}</span>
+          </div>
+        </a>
+      </div>`;
+  });
+  artsBox = artsBox.join('');
+  artsWrapper.innerHTML = artsBox;
+}
+
+function showArticle(arts) {
+  arts.map((art) => {
+    if (art.id == artId) {
+      const artWrapper = document.querySelector('.articulo-item');
+      artWrapper.innerHTML = `<h2 class="articulo-title">${art.title}</h2>` + art.content;
+    }
+  })
 }
