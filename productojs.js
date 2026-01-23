b8741fcea15dcf9a605c9161bfa349b0dcdbe283
Alejandro Monterroso <alejandromonterrosoc@gmail.com>
2026-01-23 12:37:51 -0600

    Big change in producto page
    Change in the characteristics of the product wrapper, change in the object to.
    Small changes in the footer of the index, change the phone number.

--- productojs.js
+++ productojs.js
@@ -2,48 +2,48 @@ const productos = [
   {
     id: '1',
     name: 'Pantalones',
-    prodDesc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste cupiditate enim beatae atque ut distinctio dolorem eaque ex error odio ducimus animi reprehenderit, dicta veritatis aut ab. Modi quaerat saepe deleniti quia itaque ea',
-    prodC: ['Control humedad', 'Stretch', 'Repelencia', 'Antimanchas'],
+    prodDesc: 'Nuestros pantalones combinan durabilidad, elegancia y calidad en cada costura. Diseñados para brindar comodidad y resistencia, ofrecen un estilo profesional que proyecta confianza, practicidad y una imagen impecable en cualquier entorno laboral.',
+    prodC: 'Dependiendo del producto y la selección de telas, se obtienen ventajas únicas como control de humedad, repelencia, propiedades antimanchas y tecnología stretch. Estas características aseguran comodidad, durabilidad y resistencia.',
     imgs: ['imgsCarpet/productosImgs/pantalonesImgs/DSC00279.jpg', 'imgsCarpet/productosImgs/pantalonesImgs/DSC00280.jpg', 'imgsCarpet/productosImgs/pantalonesImgs/DSC00281.jpg', 'imgsCarpet/productosImgs/pantalonesImgs/DSC00279.jpg', 'imgsCarpet/productosImgs/pantalonesImgs/DSC00280.jpg'],
-    prodRel: ['id:2', 'id:3', 'id:4', 'id:5']
+    prodRel: ['id:6', 'id:3', 'id:4', 'id:5']
   },
   {
     id: '2',
     name: 'Camisas y Blusas',
-    prodDesc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste cupiditate enim beatae atque ut distinctio dolorem eaque ex error odio ducimus animi reprehenderit, dicta veritatis aut ab. Modi quaerat saepe deleniti quia itaque ea',
-    prodC: ['Anti arrugas', 'Stretch', 'Repelencia', 'Antimanchas'],
+    prodDesc: 'Nuestras camisas y blusas están diseñadas para combinar elegancia, calidad y comodidad en cada detalle. Confeccionadas con telas resistentes que garantizan durabilidad, ofrecen una apariencia impecable incluso en jornadas intensas. Su corte moderno y funcional proyecta una imagen profesional que transmite confianza y estilo.',
+    prodC: 'Dependiendo del producto y la selección de telas, se obtienen ventajas únicas como control de humedad, repelencia, propiedades antimanchas y tecnología stretch. Estas características aseguran comodidad, durabilidad y resistencia.',
     imgs: ['imgsCarpet/productosImgs/camisasBlusasImgs/camblu5.jpg', 'imgsCarpet/productosImgs/camisasBlusasImgs/camblu1.jpg', 'imgsCarpet/productosImgs/camisasBlusasImgs/camblu2.jpg', 'imgsCarpet/productosImgs/camisasBlusasImgs/camblu3.jpg', 'imgsCarpet/productosImgs/camisasBlusasImgs/camblu4.jpg'],
-    prodRel: ['id:2', 'id:3', 'id:4', 'id:5']
+    prodRel: ['id:1', 'id:6', 'id:4', 'id:5']
   },
   {
     id: '3',
     name: 'Chalecos',
-    prodDesc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste cupiditate enim beatae atque ut distinctio dolorem eaque ex error odio ducimus animi reprehenderit, dicta veritatis aut ab. Modi quaerat saepe deleniti quia itaque ea',
-    prodC: ['Control humedad', 'Stretch', 'Repelencia', 'Antimanchas'],
+    prodDesc: 'Nuestros chalecos destacan por su comodidad, durabilidad y elegancia. Confeccionados con materiales resistentes y acabados de calidad, ofrecen un estilo versátil que proyecta profesionalismo en cualquier entorno laboral. Su diseño práctico asegura libertad de movimiento y una imagen impecable que refuerza la identidad corporativa.',
+    prodC: 'Dependiendo del producto y la selección de telas, se obtienen ventajas únicas como control de humedad, repelencia, propiedades antimanchas y tecnología stretch. Estas características aseguran comodidad, durabilidad y resistencia.',
     imgs: ['imgsCarpet/productosImgs/chalecosImgs/chaleco5.jpg', 'imgsCarpet/productosImgs/chalecosImgs/chaleco1.jpg', 'imgsCarpet/productosImgs/chalecosImgs/chaleco2.jpg', 'imgsCarpet/productosImgs/chalecosImgs/chaleco3.jpg', 'imgsCarpet/productosImgs/chalecosImgs/chaleco4.jpg'],
-    prodRel: ['id:2', 'id:3', 'id:4', 'id:5']
+    prodRel: ['id:1', 'id:2', 'id:4', 'id:5']
   },
   {
     id: '4',
     name: 'Chumpas',
-    prodDesc: 'Chumpas empresariales elegantes y duraderas, confeccionadas con algodón y tela sintética de alta calidad, brindan comodidad, resistencia y estilo profesional que refuerza la identidad y confianza de tu empresa.',
-    prodC: ['Control humedad', 'Stretch', 'Repelencia', 'Antimanchas'],
+    prodDesc: 'Chumpas diseñadas para ofrecer comodidad, durabilidad y elegancia. Con materiales resistentes y acabados de calidad, brindan protección práctica contra el clima, proyectando una imagen profesional impecable y asegurando confianza, estilo y funcionalidad en cada jornada laboral.',
+    prodC: 'Dependiendo del producto y la selección de telas, se obtienen ventajas únicas como control de humedad, repelencia, propiedades antimanchas y tecnología stretch. Estas características aseguran comodidad, durabilidad y resistencia.',
     imgs: ['imgsCarpet/productosImgs/chumpasImgs/chumpa5.jpg', 'imgsCarpet/productosImgs/chumpasImgs/chumpa1.jpg', "imgsCarpet/productosImgs/chumpasImgs/chumpa2.jpg", "imgsCarpet/productosImgs/chumpasImgs/chumpa3.jpg", "imgsCarpet/productosImgs/chumpasImgs/chumpa4.jpg"],
-    prodRel: ['id:2', 'id:3', 'id:4', 'id:5']
+    prodRel: ['id:6', 'id:3', 'id:2', 'id:5']
   },
   {
     id: '5',
     name: 'Polos',
-    prodDesc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste cupiditate enim beatae atque ut distinctio dolorem eaque ex error odio ducimus animi reprehenderit, dicta veritatis aut ab. Modi quaerat saepe deleniti quia itaque ea',
-    prodC: ['Control humedad', 'Stretch', 'Repelencia', 'Antimanchas'],
+    prodDesc: 'Nuestros polos destacan por su versatilidad, combinando comodidad y calidad en cada detalle. Confeccionados con materiales resistentes que aseguran durabilidad, ofrecen un estilo profesional adaptable a distintos entornos laborales. Su diseño moderno proyecta confianza y practicidad, reforzando la identidad corporativa con una imagen impecable.',
+    prodC: 'Dependiendo del producto y la selección de telas, se obtienen ventajas únicas como control de humedad, repelencia, propiedades antimanchas y tecnología stretch. Estas características aseguran comodidad, durabilidad y resistencia.',
     imgs: ['imgsCarpet/productosImgs/polosImgs/DSC00126.jpg', 'imgsCarpet/productosImgs/polosImgs/DSC00127.jpg', 'imgsCarpet/productosImgs/polosImgs/DSC00126.jpg', 'imgsCarpet/productosImgs/polosImgs/DSC00127.jpg', 'imgsCarpet/productosImgs/polosImgs/DSC00126.jpg'],
-    prodRel: ['id:2', 'id:3', 'id:4', 'id:5']
+    prodRel: ['id:2', 'id:3', 'id:4', 'id:1']
   },
   {
     id: '6',
     name: 'Restaurantes',
-    prodDesc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste cupiditate enim beatae atque ut distinctio dolorem eaque ex error odio ducimus animi reprehenderit, dicta veritatis aut ab. Modi quaerat saepe deleniti quia itaque ea',
-    prodC: ['Control humedad', 'Stretch', 'Repelencia', 'Antimanchas'],
+    prodDesc: 'Diseñados para entornos exigentes, nuestros textiles garantizan durabilidad, comodidad y resistencia. Con materiales de alta calidad y acabados impecables, ofrecen protección práctica y una imagen profesional que transmite confianza. Su diseño funcional asegura rendimiento constante y estilo en cada jornada culinaria.',
+    prodC: 'Dependiendo del producto y la selección de telas, se obtienen ventajas únicas como control de humedad, repelencia, propiedades antimanchas y tecnología stretch. Estas características aseguran comodidad, durabilidad y resistencia.',
     imgs: ['imgsCarpet/productosImgs/restaurantesImgs/DSC00106.jpg', 'imgsCarpet/productosImgs/restaurantesImgs/DSC00111.jpg', 'imgsCarpet/productosImgs/restaurantesImgs/DSC00115.jpg', 'imgsCarpet/productosImgs/restaurantesImgs/DSC00116.jpg', 'imgsCarpet/productosImgs/restaurantesImgs/DSC00119.jpg'],
     prodRel: ['id:2', 'id:3', 'id:4', 'id:5']
   }
@@ -83,17 +83,12 @@ function displayProduct(products) {
       
       prodText = `<h2 class="prod-title">${item.name}</h2>
         <p class="prod-description-p">${item.prodDesc}</p>
-        <div class="prod-btn-whatsapp-wrapper">
-          <a href="https://wa.me/50255125428?text=Hola%2C%20quisiera%20consultar%20sobre%20sus%20productos%20y%20servicios%2C%20mi%20nombre%20es%20" target="_blank" rel="noopener noreferrer" class="contacto-main-link-a contacto-main-link-whatsapp btn-item"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" stroke-width="2.5" stroke="currentColor" fill="none" class="duration-300 transform transition-all btn-svg" style="width: 32px; height: 32px;"><path d="M35.66 35.67a16.71 16.71 0 01-7.75-6.78.82.82 0 01.12-.95l1.55-1.69a.8.8 0 00.15-.87l-2.44-4.65s-1.08-1.65-3.29-.23c0 0-5.81 4.54-2.1 10.78s7.2 8.35 10.34 9.88c2.73 1.33 8.64 3.1 12.21-3.39 0 0 1.25-2 0-2.51-.57-.23-3.68-1.77-5-2.41a.82.82 0 00-1 .22l-1.86 2.35a.81.81 0 01-.93.25z"></path><path d="M8.86 53.49l4-11.2a23 23 0 118.64 8.8z" stroke-linecap="round"></path></svg>Whatsapp</a>
-        </div>
         <div class="prod-caract-wrapper">
           <h4 class="prod-caracteristicas-title">Caracteristicas del producto</h4>
-          <ul class="prod-caract-ul d-flex-wrapper">
-            <li class="prod-caract-li">${item.prodC[0]}</li>
-            <li class="prod-caract-li">${item.prodC[1]}</li>
-            <li class="prod-caract-li">${item.prodC[2]}</li>
-            <li class="prod-caract-li">${item.prodC[3]}</li>
-          </ul>
+          <p class="prod-description-p">${item.prodC}</p>          
+        </div>
+        <div class="prod-btn-whatsapp-wrapper">
+          <a href="https://wa.me/50255125428?text=Hola%2C%20quisiera%20consultar%20sobre%20sus%20productos%20y%20servicios%2C%20mi%20nombre%20es%20" target="_blank" rel="noopener noreferrer" class="contacto-main-link-a contacto-main-link-whatsapp btn-item"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" stroke-width="2.5" stroke="currentColor" fill="none" class="duration-300 transform transition-all btn-svg" style="width: 32px; height: 32px;"><path d="M35.66 35.67a16.71 16.71 0 01-7.75-6.78.82.82 0 01.12-.95l1.55-1.69a.8.8 0 00.15-.87l-2.44-4.65s-1.08-1.65-3.29-.23c0 0-5.81 4.54-2.1 10.78s7.2 8.35 10.34 9.88c2.73 1.33 8.64 3.1 12.21-3.39 0 0 1.25-2 0-2.51-.57-.23-3.68-1.77-5-2.41a.82.82 0 00-1 .22l-1.86 2.35a.81.81 0 01-.93.25z"></path><path d="M8.86 53.49l4-11.2a23 23 0 118.64 8.8z" stroke-linecap="round"></path></svg>Whatsapp</a>
         </div>`;      
     }
   });
